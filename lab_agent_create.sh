6270bdbb25de981531aadac14b0f262f7b7ceed8
hichem <hichemlamine@gmail.com>
2025-06-22 15:42:01 +0200

    Fix bugs

--- lab_agent_create.sh
+++ lab_agent_create.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
-ansible-playbook lab_agent_create.yml --ask-vault-pass
-
-
-
-
-
 # #!/bin/bash
+# ansible-playbook lab_agent_create.yml --ask-vault-pass
 
-# LOG_FILE="/tmp/agent_provision.log"
-# VAULT_PASS_FILE="$HOME/.vault_pass.txt"
 
-# echo "[INFO] Script lab_agent_create.sh lancé le $(date)" >> "$LOG_FILE"
 
-# echo 'password' > "$VAULT_PASS_FILE"
-# chmod 600 "$VAULT_PASS_FILE"  # sécurité
 
-# # Exécution avec log complet
-# ansible-playbook /home/hichem/jenkins-lab-02/lab_agent_create.yml \
-#     --vault-password-file "$VAULT_PASS_FILE" >> "$LOG_FILE" 2>&1
 
-# echo "[INFO] Script terminé avec code $? à $(date)" >> "$LOG_FILE"
+#!/bin/bash
+
+LOG_FILE="/tmp/agent_provision.log"
+VAULT_PASS_FILE="$HOME/.vault_pass.txt"
+
+echo "[INFO] Script lab_agent_create.sh lancé le $(date)" >> "$LOG_FILE"
+
+echo 'password' > "$VAULT_PASS_FILE"
+chmod 600 "$VAULT_PASS_FILE"  # sécurité
+
+# Exécution avec log complet
+ansible-playbook /home/hichem/jenkins-lab-02/lab_agent_create.yml \
+    --vault-password-file "$VAULT_PASS_FILE" >> "$LOG_FILE" 2>&1
+
+echo "[INFO] Script terminé avec code $? à $(date)" >> "$LOG_FILE"
--- lab_agent_remove.sh
+++ lab_agent_remove.sh
@@ -1,40 +1,40 @@
-#!/bin/bash
-
-# Vérifier que le nom de l'agent est bien passé en argument
-if [ -z "$1" ]; then
-  echo "❌ Utilisation : $0 <agent_name>"
-  exit 1
-fi
-
-AGENT_NAME="$1"
-
-
-# Exécuter le playbook avec le paramètre
-ansible-playbook lab_agent_remove.yml \
-  --ask-vault-pass \
-  -e agent_name="$AGENT_NAME"
-
-
-
-
-
-
 # #!/bin/bash
-# set -e
 
-# VAULT_PASS_FILE="$HOME/.vault_pass.txt"
-# echo 'password' > "$VAULT_PASS_FILE"
-# chmod 600 "$VAULT_PASS_FILE"
-
+# # Vérifier que le nom de l'agent est bien passé en argument
 # if [ -z "$1" ]; then
 #   echo "❌ Utilisation : $0 <agent_name>"
 #   exit 1
 # fi
 
 # AGENT_NAME="$1"
-# echo "🔍 Suppression de l’agent : $AGENT_NAME"
 
-# ansible-playbook /home/hichem/jenkins-lab-02/lab_agent_remove.yml \
-#   --vault-password-file "$VAULT_PASS_FILE" \
-#   -e agent_name="$AGENT_NAME" >> /tmp/agent_remove.log 2>&1
+
+# # Exécuter le playbook avec le paramètre
+# ansible-playbook lab_agent_remove.yml \
+#   --ask-vault-pass \
+#   -e agent_name="$AGENT_NAME"
+
+
+
+
+
+
+#!/bin/bash
+set -e
+
+VAULT_PASS_FILE="$HOME/.vault_pass.txt"
+echo 'password' > "$VAULT_PASS_FILE"
+chmod 600 "$VAULT_PASS_FILE"
+
+if [ -z "$1" ]; then
+  echo "❌ Utilisation : $0 <agent_name>"
+  exit 1
+fi
+
+AGENT_NAME="$1"
+echo "🔍 Suppression de l’agent : $AGENT_NAME"
+
+ansible-playbook /home/hichem/jenkins-lab-02/lab_agent_remove.yml \
+  --vault-password-file "$VAULT_PASS_FILE" \
+  -e agent_name="$AGENT_NAME" >> /tmp/agent_remove.log 2>&1
 
